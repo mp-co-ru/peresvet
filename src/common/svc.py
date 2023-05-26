42f06073574836dff7a358b4fe8cf33de9c3ff3b
zippy-junior <alexey@zago.ru>
2023-05-26 12:19:48 +0300

    #51 Added base class without ldap functionality

--- src/common/svc.py
+++ src/common/svc.py
@@ -3,19 +3,19 @@
 """
 import asyncio
 from functools import cached_property
-from fastapi import FastAPI
 import ldap
 
 import aio_pika
 import aio_pika.abc
 
-from src.common.hierarchy import Hierarchy
-from src.common.logger import PrsLogger
-from src.common.settings import Settings
+from .hierarchy import Hierarchy
+from .settings import Settings
+from .baseSvc import BaseSvc
 
-class Svc(FastAPI):
+
+class Svc(BaseSvc):
     """
-    Базовый класс ``Svc`` - предок всех классов-сервисов.
+    Базовый класс ``Svc`` - предок классов-сервисов.
 
     Выполняет две задачи:
 
@@ -28,37 +28,16 @@ class Svc(FastAPI):
     """
 
     def __init__(self, settings: Settings, *args, **kwargs):
-        if kwargs.get("on_startup"):
-            kwargs.append(self.on_startup)
-        else:
-            kwargs["on_startup"] = [self.on_startup]
-        if kwargs.get("on_shutdown"):
-            kwargs.append(self.on_shutdown)
-        else:
-            kwargs["on_shutdown"] = [self.on_shutdown]
-
-        super().__init__(*args, **kwargs)
-
-        self._conf = settings
-        self._logger = PrsLogger.make_logger(
-            level=settings.log["level"],
-            file_name=settings.log["file_name"],
-            retention=settings.log["retention"],
-            rotation=settings.log["rotation"]
-        )
-
+        super().__init__(settings, *args, **kwargs)
         self._hierarchy = Hierarchy(settings.ldap_url)
-
-        self._amqp_connection: aio_pika.abc.AbstractRobustConnection = None
-        self._amqp_channel: aio_pika.abc.AbstractRobustChannel = None
-        self._pub_exchange: aio_pika.abc.AbstractRobustExchange = None
 
     @cached_property
     def _config(self):
         return self._conf
 
     async def _ldap_connect(self) -> None:
-        """Функция соединения с ldap-сервером.
+        """
+        Функция соединения с ldap-сервером.
         В случае неудачи ошибка будет выведена в лог и попытки связи будут
         продолжаться с периодичностью в 5 секунд.
 
@@ -85,49 +64,17 @@ class Svc(FastAPI):
                 self._logger.error(f"Ошибка связи с сервером ldap: {ex}")
                 await asyncio.sleep(5)
 
-    async def _amqp_connect(self) -> None:
-        """Функция связи с AMQP-сервером.
-        Аналогично функции ldap-connect при неудаче ошибка будет выведена в лог
-        и попытки связи будут продолжены с периодичностью в 5 секунд.
-
-        DSN для связи с amqp-сервером указывается в переменной окружения
-        ``amqp-url``.
-
-        После установки соединения создаётся exchange с именем, указанным
-        в переменной ``svc_name`` и типом, указанным в ``pub_exchange_type``.
-        Именно этот exchange будет использоваться для публикации сообщений,
-        генерируемых сервисом.
-
-        Returns:
-            None
-        """
-        connected = False
-        while not connected:
-            try:
-                self._amqp_connection = await aio_pika.connect_robust(self._config.amqp_url)
-                self._amqp_channel = await self._amqp_connection.channel()
-                self._pub_exchange = await self._amqp_channel.declare_exchange(
-                    self._config.pub_exchange["name"],
-                    self._config.pub_exchange["type"], durable=True
-                )
-                connected = True
-
-                self._logger.info("Связь с AMQP сервером установлена.")
-
-            except aio_pika.AMQPException as ex:
-                self._logger.error(f"Ошибка связи с брокером: {ex}")
-                await asyncio.sleep(5)
-
-
     async def on_startup(self) -> None:
-        """Функция, выполняемая при старте сервиса: выполняется связь с
+        """
+        Функция, выполняемая при старте сервиса: выполняется связь с
         ldap- и amqp-серверами.
         """
         await self._ldap_connect()
         await self._amqp_connect()
 
     async def on_shutdown(self) -> None:
-        """Функция, выполняемая при остановке сервиса: разрывается связь
+        """
+        Функция, выполняемая при остановке сервиса: разрывается связь
         с ldap- и amqp-серверами.
         """
         await self._amqp_channel.close()
