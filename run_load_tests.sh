535a7709406921e03d91c6e08809f03abbc80f25
Vovaman <bd_postbox1@mail.ru>
2023-08-06 23:06:25 +0300

    #105 realized. need to test

--- run_load_tests.sh
+++ run_load_tests.sh
@@ -5,5 +5,6 @@ docker compose \
 -f docker/compose/docker-compose.rabbitmq.load_tests.yml \
 -f docker/compose/docker-compose.tags.load_tests.yml \
 -f docker/compose/docker-compose.postgresql.data_on_disk.yml \
+-f docker/compose/docker-compose.vm.load_tests.yml \
 -f docker/compose/docker-compose.locust.load_tests.yml \
 up
--- run_prepare_load_tests.sh
+++ run_prepare_load_tests.sh
@@ -4,5 +4,6 @@ docker compose \
 -f docker/compose/docker-compose.rabbitmq.load_tests.yml \
 -f docker/compose/docker-compose.tags.prepare_load_tests.yml \
 -f docker/compose/docker-compose.postgresql.data_on_disk.yml \
+-f docker/compose/docker-compose.vm.load_tests.yml \
 -f docker/compose/docker-compose.pgadmin.yml \
 up
