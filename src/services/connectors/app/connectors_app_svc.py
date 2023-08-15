826607c5e21a0679c5470a93f32ae4bea2b0a52f
zippy-junior <alexey@zago.ru>
2023-08-15 16:33:06 +0300

    #47 Added prsValueTypeCode to tag data for conenctor Added Connection manager class for further implementation of multiple connectors

--- src/services/connectors/app/connectors_app_svc.py
+++ src/services/connectors/app/connectors_app_svc.py
@@ -1,12 +1,11 @@
 import sys
 import copy
-from fastapi import APIRouter, WebSocket, WebSocketException, status, HTTPException, WebSocketDisconnect
-
+import aio_pika
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 sys.path.append(".")
 
 from src.services.connectors.app.connectors_app_settings import ConnectorsAppSettings
-from src.common import app_svc
-from src.common import hierarchy
+from src.common import app_svc, hierarchy
 
 class ConnectorsApp(app_svc.AppSvc):
     """Сервис работы с коннекторами.
@@ -62,21 +61,32 @@ class ConnectorsApp(app_svc.AppSvc):
         res = {
             "tags": []
         }
-        
-        search_query = {
-                        "id": [connector_id],
-                        "filter": {}
-                        }
 
-        async for id_, _, attributes in self._hierarchy.search(search_query):
+        tags = await self._hierarchy.search(payload={
+            "base": connector_id,
+            "scope": hierarchy.CN_SCOPE_SUBTREE,
+            "filter": {
+                "objectClass": ["prsConnectorTagData"]
+            }
+        })
+
+        for id_, _, attributes in tags:
+            tag = await self._hierarchy.search(payload={
+                "scope": hierarchy.CN_SCOPE_SUBTREE,
+                "id": attributes.get('cn')[0],
+                "filter": {
+                    "objectClass": ["prsTag"]
+                }
+            })
+            tag_id, _, tag_attr = tag[0]
+            attributes['prsValueTypeCode'] = tag_attr.get('prsValueTypeCode')[0]
             if id_:
                 res["tags"].append({
                     "tagId": id_,
                     "attributes": attributes
                 })
-
-        return res
         
+        return res
         
 settings = ConnectorsAppSettings()
 
@@ -84,15 +94,38 @@ app = ConnectorsApp(settings=settings, title="ConnectorsApp")
 
 router = APIRouter()
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+manager = ConnectionManager()
+
 @router.websocket("/{connector_id}")
 async def get_req(websocket: WebSocket, connector_id: str):
+
+    # await manager.connect(websocket)
+    await websocket.accept()
     try:
-        await websocket.accept()
         app._logger.info(f"Установлена ws-связь с коннектором: {connector_id}")
-        
+    
+        init_text = await websocket.receive_text()
         connector_tag_data = await app.get_connector_tag_data(connector_id=connector_id)
         await websocket.send_json(connector_tag_data)
-        
+
         while True:
             tag_data_json = await websocket.receive_json()
             await app._amqp_publish["main"]["exchange"].publish(
@@ -103,10 +136,11 @@ async def get_req(websocket: WebSocket, connector_id: str):
                 ),
             routing_key=app._config.publish["main"]["routing_key"]
             )
-            app._logger.info(f'Данные коннектора {connector_id} отправлены')
+            # app._logger.info(f'Данные коннектора {connector_id} отправлены')
             
-    except Exception as ex:
-        websocket.close()
-        app._logger.info(f"Разрыв связи с коннектором {connector_id}: {ex}")
+    except WebSocketDisconnect:
+        # manager.disconnect(websocket)
+        app._logger.info(f"Разрыв связи с коннектором {connector_id}")
+
 
 app.include_router(router, prefix=f"{settings.api_version}/connectors", tags=["connectors_app"])
