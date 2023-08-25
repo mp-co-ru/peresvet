84538483d8a2894b810df2bbc2901238b4e7610d
Vovaman <bd_postbox1@mail.ru>
2023-08-25 12:03:36 +0300

    #144 realized

--- run_load_tests.ssl.sh
+++ run_load_tests.ssl.sh
@@ -1,9 +1,10 @@
-# -f docker/compose/docker-compose.pgadmin.load_tests.yml \
+# -f docker/compose/docker-compose.pgadmin.yml \
 docker compose \
 -f docker/compose/docker-compose.ldap.load_tests.yml \
 -f docker/compose/docker-compose.dataStorages.load_tests.yml \
 -f docker/compose/docker-compose.rabbitmq.load_tests.yml \
--f docker/compose/docker-compose.tags.load_tests.yml \
+-f docker/compose/docker-compose.tags.load_tests.ssl.yml \
 -f docker/compose/docker-compose.postgresql.data_on_disk.yml \
--f docker/compose/docker-compose.locust.load_tests.get_data_psql.yml \
+-f docker/compose/docker-compose.vm.load_tests.yml \
+-f docker/compose/docker-compose.locust.load_tests.yml \
 up
