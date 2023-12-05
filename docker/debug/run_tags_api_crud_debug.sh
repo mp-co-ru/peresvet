659364374ffc08e51dad285a873c7b24904e661d
Vovaman <bd_postbox1@mail.ru>
2023-12-05 11:56:00 +0300

    #169 in progress

--- docker/debug/run_tags_api_crud_debug.sh
+++
@@ -1,1 +0,0 @@
-docker compose -f ../compose/docker-compose.ldap.yml -f ../compose/docker-compose.rabbitmq.yml -f compose/docker-compose.tags_api_crud.uvicorn.yml -f compose/docker-compose.tags_api_crud.debug.yml up --build -d
