b6ebc7fcae57632eeac7081775545f0da7b380aa
Vovaman <bd_postbox1@mail.ru>
2023-12-20 18:11:28 +0300

    #186 new base class for dataStorages are added

---
+++ src/services/dataStorages/app/dataStorages_app_base_settings.py
@@ -0,0 +1,69 @@
+from typing import List
+from src.common.svc_settings import SvcSettings
+
+class DataStoragesAppBaseSettings(SvcSettings):
+    #: имя сервиса
+    svc_name: str = "dataStorages_app_base"
+    #: строка коннекта к RabbitMQ
+    amqp_url: str = "amqp://prs:Peresvet21@rabbitmq/"
+
+    # описание обменников, в которые сервис будет публиковать свои сообщения
+    # наиболее часто это всего один обменник, описанный в ключе "main"
+    # информацию об обменниках и сообщениях см. в документации на каждый
+    # конкретный сервис
+    publish: dict = {
+        #: главный обменник
+        "main": {
+            #: имя обменника
+            "name": "peresvet",
+            #: тип обменника
+            "type": "direct",
+            #: routing_key, с которым будут публиковаться сообщения,
+            # вычисляется во время работы сервиса: это - tag_id или alert_id
+            #"routing_key": ["dataStorages_app_postgresql"]
+        }
+    }
+
+    # описание обменников, из которых сервис получает сообщения
+    # информацию об обменниках и сообщениях см. в документации на каждый
+    # конкретный сервис
+    consume: dict = {
+        "queue_name": "dataStorages_app_base_consume",
+        "exchanges": {
+            "main": {
+                #: имя обменника
+                "name": "peresvet",
+                #: тип обменника
+                "type": "direct",
+                #: привязка для очереди
+                "routing_key": ["dataStorages_model_crud_publish"]
+            },
+            "tags": {
+                "name": "peresvet",
+                "type": "direct",
+                #: привязка для очереди
+                # привязка вычисляется во время работы сервиса
+            },
+            "alerts": {
+                "name": "peresvet",
+                "type": "direct",
+                #: привязка для очереди
+                # привязка вычисляется во время работы сервиса
+            }
+        }
+    }
+
+    #: строка коннекта к OpenLDAP
+    ldap_url: str = "ldap://ldap:389/cn=prs????bindname=cn=admin%2ccn=prs,X-BINDPW=Peresvet21"
+
+    # код типа хранилища: 0 - Postgresql, 1 - victoriametrics
+    datastorage_type: int = 0
+    # в этом параметре указываются коды хранилищ, которые будет обслуживать
+    # данный сервис
+    # если коды не указаны, то будут обслуживаться все хранилища заданного типа
+    datastorages_id: List[str] = []
+
+    # периодичность накопления кэша данных, секунды
+    cache_data_period: int = 30
+
+    cache_url: str = "redis://redis:6379?decode_responses=True&protocol=3"
