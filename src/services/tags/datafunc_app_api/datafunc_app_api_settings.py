b6ad96c6db9f31fc14f0a76aef617ba1c478977a
Vovaman <bd-postbox1@mail.ru>
2024-11-08 14:51:29 +0300

    #86 pandas changed to datafunc

--- src/services/tags/datafunc_app_api/datafunc_app_api_settings.py
+++ src/services/tags/datafunc_app_api/datafunc_app_api_settings.py
@@ -1,10 +1,8 @@
 from src.common.svc_settings import SvcSettings
 
-class AlertsAppAPISettings(SvcSettings):
-
+class DatafuncAppAPISettings(SvcSettings):
     #: имя сервиса
-    svc_name: str = "alerts_app_api"
+    svc_name: str = "datafunc_app_api"
     #: версия API
     api_version: str = "/v1"
-
     
