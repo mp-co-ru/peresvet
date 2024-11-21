165ed189bfa83c76a52a42f436db07b7ac1ffbef
Vovaman <bd-postbox1@mail.ru>
2024-11-21 21:27:44 +0300

    #116 insert sleep

--- run_one_app_ssl_letsencrypt_generate_certificates.sh
+++ run_one_app_ssl_letsencrypt_generate_certificates.sh
@@ -16,8 +16,8 @@ fi
 
 sed -i "s/NGINX_HOST=.*/NGINX_HOST=$srv/" docker/compose/.cont_one_app.env
 docker compose --env-file docker/compose/.cont_one_app.env \
-    -f docker/compose/docker-compose.nginx.ssl_letsencrypt_generate_certificates.yml up &
-
+    -f docker/compose/docker-compose.nginx.ssl_letsencrypt_generate_certificates.yml up
+sleep 15
 docker compose --env-file docker/compose/.cont_one_app.env \
     -f docker/compose/docker-compose.certbot.ssl_letsencrypt_generate_certificates.yml run \
     --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ -d $srv
