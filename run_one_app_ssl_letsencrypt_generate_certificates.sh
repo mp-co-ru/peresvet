f2c1077ae34aa6e009d757455eebbe24cd03f82d
Vovaman <bd-postbox1@mail.ru>
2024-11-21 17:10:21 +0300

    #116 add script for certificates generating

---
+++ run_one_app_ssl_letsencrypt_generate_certificates.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Скрипт запускает контейнеры с сервисами платформы. Протокол - HTTPS.
+# Предназначен для запуска на сервере, доступном из интернета, 
+# так как сертификаты генерируются с помощью Let's Encrypt.
+# 
+# в первом аргументе можно задать имя сервера, в качестве которого по умолчанию
+# принимается имя текущего сервера
+
+#-f docker/compose/docker-compose.grafana.yml \
+
+srv=$HOSTNAME
+if [ -n "$1" ]
+then
+    srv=$1
+fi
+
+sed -i "s/NGINX_HOST=.*/NGINX_HOST=$srv/" docker/compose/.cont_one_app.env
+docker compose --env-file docker/compose/.cont_one_app.env \
+    -f docker/compose/docker-compose.nginx.one_app_ssl_letsencrypt_first_run.yml \
+    up
+docker compose --env-file docker/compose/.cont_one_app.env \
+    -f docker/compose/docker-compose.nginx.one_app_ssl_letsencrypt_first_run.yml \
+    run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ -d $srv
