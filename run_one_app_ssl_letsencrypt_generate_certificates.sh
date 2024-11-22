30a2f087074c498cac67f39634261ae1aa4bea3a
Vovaman <bd-postbox1@mail.ru>
2024-11-22 11:39:01 +0300

    #116 fixes

--- run_one_app_ssl_letsencrypt_generate_certificates.sh
+++ run_one_app_ssl_letsencrypt_generate_certificates.sh
@@ -16,11 +16,10 @@ fi
 
 sed -i "s/NGINX_HOST=.*/NGINX_HOST=$srv/" docker/compose/.cont_one_app.env
 docker compose --env-file docker/compose/.cont_one_app.env \
-    -f docker/compose/docker-compose.nginx.ssl_letsencrypt_generate_certificates.yml up
-sleep 15
+    -f docker/compose/docker-compose.nginx.ssl_letsencrypt_generate_certificates.yml up \
+&& \
 docker compose --env-file docker/compose/.cont_one_app.env \
     -f docker/compose/docker-compose.certbot.ssl_letsencrypt_generate_certificates.yml run \
     --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ -d $srv
 
-docker stop nginx_one_app
-docker rm nginx_one_app
+docker stop nginx_one_app && docker rm nginx_one_app
