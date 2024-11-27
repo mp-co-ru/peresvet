92bd6c4457ea06e3cb5da887c3e28a3ac1a63ac3
Vovaman <bd-postbox1@mail.ru>
2024-11-27 21:22:19 +0300

    #132 search connectors

--- src/services/connectors/api_crud/connectors_api_crud_settings.py
+++ src/services/connectors/api_crud/connectors_api_crud_settings.py
@@ -4,4 +4,9 @@ class ConnectorsAPICRUDSettings(APICRUDSettings):
 
     #: имя сервиса
     svc_name: str = "connectors_api_crud"
+
+    hierarchy: dict = {
+        #: класс экземпляров сущности в иерархии
+        "class": "prsConnector"
+    }
     
